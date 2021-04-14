40c919d04233ed01afdd88e1d94a7e5228d1815b
Akos1993 <77202075+Akos1993@users.noreply.github.com>
2021-04-14 17:49:05 +0200

    Add files via upload

--- Questions.java
+++ Questions.java
@@ -9,23 +9,16 @@ public class Questions {
     public void fillQuestions(String question, List<String> answers, String right) {
         Map<String,Boolean> rightAnswer = new HashMap<>();
         questions.add(question);
-        for (int i = 0; i < answers.size(); i++) {
-            if (answers.get(i).equals(right)) {
-                rightAnswer.put(answers.get(i),true);
-            }else{
-                rightAnswer.put(answers.get(i),false);
+        for (String answer : answers) {
+            if (answer.equals(right)) {
+                rightAnswer.put(answer, true);
+            } else {
+                rightAnswer.put(answer, false);
             }
         }
         this.answers.put(question,rightAnswer);
     }
 
-    public void setQuestions(List<String> questions) {
-        this.questions = questions;
-    }
-
-    public void setAnswers(Map<String, Map<String, Boolean>> answers) {
-        this.answers = answers;
-    }
 }
 class QuestionMain {
 
@@ -37,27 +30,27 @@ class QuestionMain {
 
         while (!isExit) {
             List<String> answers = new ArrayList<>();
-            System.out.println("Provide the "+ i + "Question");
+            System.out.println("Provide the "+ i + ". " + "Question");
             String question = in.nextLine();
             in.nextLine();
             String right ="";
             int j = 0;
             while(!isExit) {
-                System.out.println("Provide the answers");
+                System.out.println("Provide answer no." + (j+1));
                 answers.add(in.nextLine());
                 in.nextLine();
                 System.out.println("Is this the correct answer? 1 yes / 0 no");
                 if (in.nextByte() == 1) {
                     right = answers.get(j);
                 }
-                System.out.println("Want to add more answers?");
-                isExit = in.nextByte() == 1;
+                System.out.println("Want to add more answers? 1 yes / 0 no");
+                isExit = in.nextByte() == 0;
                 in.nextLine();
                 j++;
             }
             questions.fillQuestions(question,answers,right);
             i++;
-            System.out.println("Want to put more questions?");
+            System.out.println("Want to put more questions? 1 yes / 0 no");
             isExit = in.nextByte() == 1;
         }
 
