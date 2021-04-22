dec845a8f46c4e9f78dcc181d6893ed7078c89e3
Akos1993 <77202075+Akos1993@users.noreply.github.com>
2021-04-22 08:56:00 +0200

    Add files via upload

--- QuestionsDao.java
+++ QuestionsDao.java
@@ -1,31 +1,50 @@
 package com.akos;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.function.Consumer;
 
-public class QuestionsDao implements Dao<Questions> {
+public class QuestionsDao implements Dao<String> {
 
     private EntityManager entityManager;
 
     @Override
-    public List<Questions> getAll() {
-        Query query = entityManager.createQuery("SELECT e FROM e");
+    public List<String> getAll() {
+        Query query = entityManager.createQuery("SELECT * FROM Test_Questions");
         return query.getResultList();
     }
 
     @Override
-    public void save(Questions questions) {
-        entityManager.persist(questions);
+    public void save(String questions) {
+        executeInsideTransaction(entityManager -> entityManager.persist(questions);
     }
 
     @Override
-    public void update(Questions questions) {
-        entityManager.merge(questions);
+    public void update(String questions) {
+        executeInsideTransaction(entityManager -> entityManager.merge(questions);
     }
 
     @Override
-    public void delete(Questions questions) {
-        entityManager.remove(questions);
+    public void delete(String questions) {
+        executeInsideTransaction(entityManager -> entityManager.remove(questions));
+    }
+
+    public void updateID(int testID) {
+
+    }
+
+    private void executeInsideTransaction(Consumer<EntityManager> action) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        }
     }
 }
